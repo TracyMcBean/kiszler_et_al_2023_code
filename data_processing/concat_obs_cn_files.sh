4252a77413018b527d4814aeb00d762b19a3ffff
Tracy Kiszler <33286976+TracyMcBean@users.noreply.github.com>
2023-06-05 11:46:25 +0200

    delete paths in concat_obs_cn_files.sh

--- data_processing/concat_obs_cn_files.sh
+++ data_processing/concat_obs_cn_files.sh
@@ -5,13 +5,14 @@
 # 19.02.2021, TK
 # Modifications: 03.08.2021, TK: Concatenate instead of mergetime and switch to hatpro.
 
-cd /work/tkiszler/data/
+cd /work/
 
 YEAR=2020
 MONTHS=( "08" "09" "10" "11" "12" )
 DAYS=( "01" "02" "03" "04" "05" "06" "07" "08" "09" "10" "11" "12" "13" "14" "15" "16" "17" "18" "19" "20" "21" "22" "23" "24" "25" "26" "27" "28" "29" "30" "31" )
 
-CN_PATH="/data/hatpro/jue/cloudnet/ny-alesund/products/classification/${YEAR}"
+# path where cn classifications are
+CN_PATH="classification/${YEAR}"
 
 for m in ${MONTHS[@]}
 do
@@ -24,9 +25,9 @@ do
 
 	#if [ ${d} == ${DAYS[0]} ] && [ ${m} == ${MONTHS[0]} ]
 	#then
-	#    cp ${CN_FILE} /net/cierzo/tkiszler/data/2020/nyalesund_obs_cn_classification.nc
+	#    cp ${CN_FILE} /nyalesund_obs_cn_classification.nc
 	#else
-	#    cdo -O mergetime /net/cierzo/tkiszler/data/2020/nyalesund_obs_cn_classification.nc ${CN_FILE} /net/cierzo/tkiszler/data/2020/nyalesund_obs_cn_classification.nc
+	#    cdo -O mergetime /nyalesund_obs_cn_classification.nc ${CN_FILE} nyalesund_obs_cn_classification.nc
 	#fi    
         echo ${CN_FILE}
     done
