631488b22814a93f548ce5436870af3879b1e763
Tracy Kiszler <33286976+TracyMcBean@users.noreply.github.com>
2023-06-05 11:44:57 +0200

    Update concat_model_cn_files.sh

--- data_processing/concat_model_cn_files.sh
+++ data_processing/concat_model_cn_files.sh
@@ -11,7 +11,8 @@ YEAR=2020
 MONTHS=( "08" "09" "10" "11" "12" )
 DAYS=( "01" "02" "03" "04" "05" "06" "07" "08" "09" "10" "11" "12" "13" "14" "15" "16" "17" "18" "19" "20" "21" "22" "23" "24" "25" "26" "27" "28" "29" "30" "31" )
 
-ICON_LEM_PATH="/data/inscape/icon/experiments/nyalesund/testbed/"
+# model data path
+ICON_LEM_PATH=""
 
 for m in ${MONTHS[@]}
 do
