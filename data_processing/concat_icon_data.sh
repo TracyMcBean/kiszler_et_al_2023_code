346ac2136567edabb8b76b2b13fc545750c202e9
Tracy Kiszler <33286976+TracyMcBean@users.noreply.github.com>
2023-06-05 11:44:26 +0200

    Update concat_icon_data.sh

--- data_processing/concat_icon_data.sh
+++ data_processing/concat_icon_data.sh
@@ -11,8 +11,10 @@ YEAR=2020
 MONTHS=( "08" "09" "10" "11" "12" )
 DAYS=( "01" "02"  "03" "04" "05" "06" "07" "08" "09" "10" "11" "12" "13" "14" "15" "16" "17" "18" "19" "20" "21" "22" "23" "24" "25" "26" "27" "28" "29" "30" "31" )
 
-EXP_PATH="/data/inscape/icon/experiments/nyalesund/testbed"
-WORK_PATH="/net/hegoa/tkiszler/data/model/"
+# meteogram files location
+EXP_PATH=""
+#current working path
+WORK_PATH=""
 
 cd ${WORK_PATH}
 
